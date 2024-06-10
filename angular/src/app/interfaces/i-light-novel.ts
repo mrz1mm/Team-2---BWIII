401bfbbd1ab8fb7f156197f931139b50a5acd2f0
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-06-10 14:04:57 +0200

    refactor: Update ILightNovel interface with additional properties and nested story structure

--- angular/src/app/interfaces/i-light-novel.ts
+++ angular/src/app/interfaces/i-light-novel.ts
@@ -1,2 +1,25 @@
 export interface ILightNovel {
+  id: number;
+  userId: number;
+  slug: string;
+  title: string;
+  plot: string;
+  description: string;
+  genre: string;
+  author: string;
+  image_url: string;
+  created_at: string;
+  story: {
+    part1: string;
+    part2: {
+      part2a: string;
+      part2b: string;
+    };
+    part3: {
+      part3a: string;
+      part3b: string;
+      part3c: string;
+      part3d: string;
+    };
+  };
 }
--- angular/src/app/services/lightnovel.service.ts
+++ angular/src/app/services/lightnovel.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { SearchService } from './search.service';
+import { UsersService } from './users.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LightnovelService {
+  constructor(
+    private searchSvc: SearchService,
+    private usersSvc: UsersService
+  ) {}
 
-  constructor() { }
+  private lightNovelsSubject = new BehaviorSubject<iLightNovel[]>([]);
+  private lightNovels$ = this.lightNovelsSubject.asObservable();
 }
