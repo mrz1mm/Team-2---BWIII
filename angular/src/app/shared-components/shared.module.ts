5ed53bdc3722a5988080c958bc01cf6fea21fb24
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-06-10 17:08:40 +0200

    refactor: Update user module to use shared user card component

--- angular/src/app/shared-components/shared.module.ts
+++ angular/src/app/shared-components/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserCardComponent } from './user-card/user-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     UserCardComponent
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,NgbModule],
   exports: [
     UserCardComponent
   ],
