2b30ff0fd540cc332a5bbc658add1ebff0a91850
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-06-10 15:19:38 +0200

    chore: Update json-server dependency to version 0.17.4

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './main-components/header/header.component';
+import { FooterComponent } from './main-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- angular/src/app/services/users.service.ts
+++ angular/src/app/services/users.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private apiUrl = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getUser(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createUser(user: any): Observable<any> {
+    return this.http.post(this.apiUrl, user);
+  }
+
+  updateUser(id: number, user: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  upgradeUser(id: number): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}`, { role: 'sub-admin' });
+  }
+
+  downgradeUser(id: number): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${id}`, { role: 'auth' });
+  }
 }
