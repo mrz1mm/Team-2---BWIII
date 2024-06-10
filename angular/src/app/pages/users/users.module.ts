aff71aa1df2a04d3fc1c2cfb618e1e716bc98c25
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-06-10 17:05:16 +0200

    refactor: Update user module to use shared

--- angular/src/app/pages/users/users.module.ts
+++ angular/src/app/pages/users/users.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserCardModule } from '../../shared-components/user-card/user-card.module';
+import { SharedModule } from '../../shared-components/shared.module';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 @NgModule({
   declarations: [UsersComponent],
-  imports: [CommonModule, UsersRoutingModule,UserCardModule],
+  imports: [CommonModule, UsersRoutingModule, SharedModule],
 })
 export class UsersModule {}
--- angular/src/app/shared-components/shared.module.ts
+++ angular/src/app/shared-components/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
   declarations: [
+    UserCardComponent
   ],
   imports: [CommonModule, RouterModule],
-  exports: [],
+  exports: [
+    UserCardComponent
+  ],
 })
 export class SharedModule {}
