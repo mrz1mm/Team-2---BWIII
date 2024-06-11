33925b1ea081c9343e08e64820cfc49b92853f53
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-06-11 12:26:12 +0200

    aggiornamento_profilo

--- angular/src/app/pages/profile/profile.component.ts
+++ angular/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { iUser } from '../../auth/interfaces/i-user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  constructor(private profile_svc: AuthService) {}
 
+  user!: iUser;
+
+  ngOnInit() {
+    this.Profile()
+  }
+
+  Profile = () => {
+    this.profile_svc.user$.subscribe((data) => {
+      if (data) {
+        this.user = data;
+      }
+    });
+  };
 }
