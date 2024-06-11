6a5b87afe0f8daedc389d24bb52cc82bc260fcee
Simone Attanasio <161967392+mrz1mm@users.noreply.github.com>
2024-06-11 20:42:47 +0200

    refactor: Improve genre button functionality in HomeComponent

--- angular/src/app/pages/home/home.component.ts
+++ angular/src/app/pages/home/home.component.ts
@@ -19,11 +19,14 @@ export class HomeComponent {
   ) {}
 
   lightNovelsArray: iLightNovel[] = [];
+  filteredLightNovelsArray: iLightNovel[] = [];
   genres: string[] = [];
+  activeIndex: number | null = null;
 
   ngOnInit() {
     this.lightNovelSvc.lightNovels$.subscribe((lightNovels) => {
       this.lightNovelsArray = lightNovels;
+      this.filteredLightNovelsArray = this.lightNovelsArray;
       this.genres = this.lightNovelSvc.getAllGenres();
     });
   }
@@ -47,6 +50,24 @@ export class HomeComponent {
   }
 
   selectedGenre(genre: string) {
-    this.lightNovelsArray = this.lightNovelSvc.getLightNovelsByGenre(genre);
+    this.filteredLightNovelsArray =
+      this.lightNovelSvc.getLightNovelsByGenre(genre);
+  }
+
+  toggleActive(index: number) {
+    if (this.activeIndex === index) {
+      this.activeIndex = null; // Deseleziona se clicca sullo stesso bottone
+    } else {
+      this.activeIndex = index;
+    }
+  }
+
+  handleButtonClick(index: number, genre: string) {
+    this.toggleActive(index);
+    if (this.activeIndex !== null) {
+      this.selectedGenre(genre);
+    } else {
+      this.filteredLightNovelsArray = this.lightNovelsArray; // Reset della lista se nessun genere è attivo
+    }
   }
 }
